adcf225f090d74750f958e0c5f1afafe54af7987
Meoltieki <88784165+Meoltieki@users.noreply.github.com>
2021-12-09 12:02:12 +0100

    funktiniert halbwegs

--- js/game.js
+++ js/game.js
@@ -1,17 +1,22 @@
 const KEY_CODE_LEFT = 37;
 const KEY_CODE_RIGHT = 39;
 const KEY_CODE_SPACE = 32;
+const KEY_CODE_UP = 38;
+const KEY_CODE_DOWN = 40;
 
 
 const GAME_WIDTH = 800;
 const GAME_HEIGHT = 600;
-
+const PLAYER_WIDTH = 20;
 
 const GAME_STATE = {
 
     leftPressed: false,
     rightPressed: false,
+    upPressed: false,
+    downPressed: false,
     spacePressed: false,
+
     playerX: 0,
     playerY: 0,
 
@@ -19,6 +24,18 @@ const GAME_STATE = {
 function setPosition($el, x, y){
 
     $el.style.transform = `translate(${x}px, ${y}px)`;
+
+}
+
+function clamp(v, min, max){
+
+    if (v<min){
+        return min;
+    } else if (v<max){
+        return max;
+    }else {
+        return v;
+    }
 
 }
 
@@ -47,7 +64,19 @@ function updatePlayer(){
     if (GAME_STATE.rightPressed){
         GAME_STATE.playerX += 5;
     }
-
+    if (GAME_STATE.leftPressed){
+        GAME_STATE.playerX -= 5;
+    }
+    if (GAME_STATE.rightPressed){
+        GAME_STATE.playerX += 5;
+    }
+    if (GAME_STATE.upPressed){
+        GAME_STATE.playerY -= 5;
+    }
+    if (GAME_STATE.downPressed){
+        GAME_STATE.playerY += 5;
+    }
+    GAME_STATE.playerX = clamp(GAME_STATE.playerX, PLAYER_WIDTH, GAME_WIDTH - PLAYER_WIDTH);
     const $player = document.querySelector(".player");
     setPosition($player, GAME_STATE.playerX, GAME_STATE.playerY);
 }
@@ -72,6 +101,14 @@ if (e.keyCode === KEY_CODE_LEFT){
 
     GAME_STATE.spacePressed = true;
 
+    }else if(e.keyCode === KEY_CODE_UP) {
+
+        GAME_STATE.upPressed = true;
+    
+    } else if(e.keyCode === KEY_CODE_DOWN) {
+
+        GAME_STATE.downPressed = true;
+    
     }
 }
 
@@ -89,6 +126,16 @@ function onKeyUp(e) {
         GAME_STATE.spacePressed = false;
     
     }
+    else if(e.keyCode === KEY_CODE_UP) {
+
+        GAME_STATE.upPressed = false;
+    
+    } else if(e.keyCode === KEY_CODE_DOWN) {
+
+        GAME_STATE.downPressed = false;
+    
+    }
+    console.log(e);
 }
 
 init ();
