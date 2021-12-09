9ad1441e94fee94f714d8a959d7021c4e9f52fb0
Meoltieki <88784165+Meoltieki@users.noreply.github.com>
2021-12-09 11:26:53 +0100

    moves rl

--- js/game.js
+++ js/game.js
@@ -1,9 +1,17 @@
+const KEY_CODE_LEFT = 37;
+const KEY_CODE_RIGHT = 39;
+const KEY_CODE_SPACE = 32;
+
+
 const GAME_WIDTH = 800;
-const GAME_HEIGHT = 550;
+const GAME_HEIGHT = 600;
 
 
 const GAME_STATE = {
 
+    leftPressed: false,
+    rightPressed: false,
+    spacePressed: false,
     playerX: 0,
     playerY: 0,
 
@@ -32,4 +40,59 @@ function init() {
     createPlayer($container);
 }
 
+function updatePlayer(){
+    if (GAME_STATE.leftPressed){
+        GAME_STATE.playerX -= 5;
+    }
+    if (GAME_STATE.rightPressed){
+        GAME_STATE.playerX += 5;
+    }
+
+    const $player = document.querySelector(".player");
+    setPosition($player, GAME_STATE.playerX, GAME_STATE.playerY);
+}
+
+function update() {
+
+    updatePlayer();
+    window.requestAnimationFrame(update);
+}
+
+function onKeyDown(e) {
+if (e.keyCode === KEY_CODE_LEFT){
+
+   GAME_STATE.leftPressed = true;
+
+} else if(e.keyCode === KEY_CODE_RIGHT) {
+
+   GAME_STATE.rightPressed = true;
+
+   }
+   else if(e.keyCode === KEY_CODE_SPACE) {
+
+    GAME_STATE.spacePressed = true;
+
+    }
+}
+
+function onKeyUp(e) {
+    if (e.keyCode === KEY_CODE_LEFT){
+    
+       GAME_STATE.leftPressed = false;
+    
+    } else if(e.keyCode === KEY_CODE_RIGHT) {
+    
+       GAME_STATE.rightPressed = false;
+    
+       } else if(e.keyCode === KEY_CODE_SPACE) {
+    
+        GAME_STATE.spacePressed = false;
+    
+    }
+}
+
 init ();
+
+window.addEventListener("keydown", onKeyDown);
+window.addEventListener("keyup", onKeyUp);
+window.requestAnimationFrame(update);
